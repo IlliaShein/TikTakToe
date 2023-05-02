d67dfe8200e907dff2a936679df2c85e330d404d
IlliaShein <illiasheinillia@gmail.com>
2023-05-02 21:35:14 +0300

    Added AreYouSureWindow.xaml window

---
+++ TilTakToe/XAML/Windows/AreYouSureWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using TilTakToe.Classes.StaticClasses;
+
+namespace TilTakToe.XAML.Windows
+{
+    public partial class AreYouSureWindow : Window
+    {
+        public AreYouSureWindow()
+        {
+            InitializeComponent();
+        }
+        private void ExitButon_Click(object sender, RoutedEventArgs e)
+        {
+            GeneralMethods.CloseWindow();
+        }
+
+        private void MinimizeButon_Click(object sender, RoutedEventArgs e)
+        {
+            GeneralMethods.MinimizeWindow(areYouSureWindow);
+        }
+
+        private void MenuBorder_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            GeneralMethods.HoldAndMoveWindow(areYouSureWindow);
+        }
+    }
+}
